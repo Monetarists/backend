b921b20942ad3ad2be7f56e9fcd844509b5d18d3
Joakim Jarvinen <jarjo@devcore.se>
2022-05-27 20:13:14 +0200

    DB Migrations

--- XIVMarketBoard_Api/Migrations/20220115002653_initial.Designer.cs
+++ XIVMarketBoard_Api/Migrations/20220115002653_initial.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using XIVMarketBoard_Backend.Data;
+using XIVMarketBoard_Api.Data;
 
 #nullable disable
 
---
+++ XIVMarketBoard_Api/Migrations/20220213161947_AddedIngredientAndAmount.cs
@@ -0,0 +1,249 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XIVMarketBoard_Api.Migrations
+{
+    public partial class AddedIngredientAndAmount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Recipes_RecipeId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Jobs_JobName",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_JobName",
+                table: "Recipes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Jobs",
+                table: "Jobs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_RecipeId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "JobName",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "RecipeId",
+                table: "Items");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AmountResult",
+                table: "Recipes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "jobId",
+                table: "Recipes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobName",
+                table: "Jobs",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Jobs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "CanBeCrafted",
+                table: "Items",
+                type: "tinyint(1)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Jobs",
+                table: "Jobs",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    RecipeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "ItemId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "RecipeId");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_ItemId",
+                table: "Recipes",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_jobId",
+                table: "Recipes",
+                column: "jobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_ItemId",
+                table: "Ingredients",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_RecipeId",
+                table: "Ingredients",
+                column: "RecipeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Items_ItemId",
+                table: "Recipes",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "ItemId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Jobs_jobId",
+                table: "Recipes",
+                column: "jobId",
+                principalTable: "Jobs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Items_ItemId",
+                table: "Recipes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Jobs_jobId",
+                table: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_ItemId",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_jobId",
+                table: "Recipes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Jobs",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "AmountResult",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "jobId",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Jobs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "JobName",
+                table: "Recipes",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobName",
+                table: "Jobs",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "CanBeCrafted",
+                table: "Items",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RecipeId",
+                table: "Items",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Jobs",
+                table: "Jobs",
+                column: "JobName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_JobName",
+                table: "Recipes",
+                column: "JobName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_RecipeId",
+                table: "Items",
+                column: "RecipeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Recipes_RecipeId",
+                table: "Items",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "RecipeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Jobs_JobName",
+                table: "Recipes",
+                column: "JobName",
+                principalTable: "Jobs",
+                principalColumn: "JobName");
+        }
+    }
+}
