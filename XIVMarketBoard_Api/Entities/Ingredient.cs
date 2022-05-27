065eba2ed51cbd4685eff950e577751b44fff310
Joakim Jarvinen <jarjo@devcore.se>
2022-05-27 20:11:56 +0200

    added m to m table for ingredients

---
+++ XIVMarketBoard_Api/Entities/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace XIVMarketBoard_Api.Entities
+{
+    public class Ingredient
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public Item Item { get; set; }
+    }
+}
