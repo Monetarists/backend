c0172a9fdbe2f4f49785f5625937c5c64978a223
Joakim Jarvinen <joakim@jarvinen.se>
2022-05-31 23:00:11 +0200

    added controller for universalis

---
+++ XIVMarketBoard_Api/UniversalisApiController.cs
@@ -0,0 +1,22 @@
+﻿using XIVMarketBoard_Api.Entities;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using XIVMarketBoard_Api.Data;
+using System;
+using Microsoft.EntityFrameworkCore;
+namespace XIVMarketBoard_Api
+{
+    public class UniversalisApiController
+    {
+        public static List<MbPost> getPostsForItem()
+        {
+            List<MbPost> list = new List<MbPost>();
+            using (var xivContext = new XivDbContext())
+            {
+                // Perform data access using the context
+            }
+            return list;
+        }
+    }
+}
