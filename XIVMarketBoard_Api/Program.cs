b1ee0d7338bdfadfb5c27c6af88e6aeb25692bc6
Joakim Jarvinen <joakim@jarvinen.se>
2022-06-20 18:45:52 +0200

    added swagger to regular operation

--- XIVMarketBoard_Api/Program.cs
+++ XIVMarketBoard_Api/Program.cs
@@ -46,13 +46,12 @@ builder.Services.AddTransient<IXivApiRepository, XivApiRepository>();
 builder.Services.AddTransient<IDbController, DbController>();
 builder.Services.AddDbContext<XivDbContext>();
 var app = builder.Build();
-
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 }
 
 //app.UseHttpsRedirection();
