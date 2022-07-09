9c3ada6eda16bc44611b9b50cd1a8338e6e6a5ed
Joakim Jarvinen <joakim@jarvinen.se>
2022-07-09 13:17:54 +0200

    Added nullchecks for ingredient name to apicontroller added nullable to responsedto variables added using XIVMarketBoard_Api to program so it can access the dto

--- XIVMarketBoard_Api/ResponseDto.cs
+++ XIVMarketBoard_Api/ResponseDto.cs
@@ -5,11 +5,11 @@ namespace XIVMarketBoard_Api
     public class ResponseDto
     {
         public const string noItemsMessage = "The Api Returned No Items";
-        public string message { get; set; }
+        public string? message { get; set; }
         public IEnumerable<Item>? Items { get; set; }
         public IEnumerable<Recipe>? Recipes { get; set; }
         public IEnumerable<UniversalisEntry>? UniversalisEntry { get; set; }
-        public Dictionary<int, string> dict { get; set; }
+        public Dictionary<int, string>? dict { get; set; }
 
 
     }
