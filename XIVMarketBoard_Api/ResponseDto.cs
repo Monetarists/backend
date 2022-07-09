d90049855817757ad9630ef112b957bdfdd86435
Joakim Jarvinen <joakim@jarvinen.se>
2022-07-09 13:10:45 +0200

    renamed apiresponse to responsedto moved responsedto from models to root removed unnecessary using staments

--- XIVMarketBoard_Api/ResponseDto.cs
+++ XIVMarketBoard_Api/ResponseDto.cs
@@ -1,13 +1,15 @@
 ﻿using XIVMarketBoard_Api.Entities;
-namespace XIVMarketBoard_Api.Repositories.Models
+
+namespace XIVMarketBoard_Api
 {
-    public class ApiResponse
+    public class ResponseDto
     {
         public const string noItemsMessage = "The Api Returned No Items";
         public string message { get; set; }
         public IEnumerable<Item>? Items { get; set; }
         public IEnumerable<Recipe>? Recipes { get; set; }
         public IEnumerable<UniversalisEntry>? UniversalisEntry { get; set; }
+        public Dictionary<int, string> dict { get; set; }
 
 
     }
